c7fccec64b5ac00bdf435fd83bc9cf4d01c056d4
Dan Thomas <dan.thomas@ipublic.org>
2010-12-23 15:07:21 -0500

    add filter_internal_properties method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,9 @@ module ApplicationHelper
     Time.local(year.to_i, month.to_i, day.to_i)
   end
 
+  # remove couchdb and openmedia internal prooperties
+  def filter_internal_properties(properties)
+    properties.reject! { |k,v| ["_id", "_rev", "import_id", "couchrest-type", "created_at", "updated_at"].include?(k.to_s) }
+  end
+
 end

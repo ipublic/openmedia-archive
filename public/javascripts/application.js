771845f3e6f1272fe5766d3c0f3c1ad36f189f92
Dan Thomas <dan.thomas@ipublic.org>
2010-12-27 17:28:41 -0500

    Added Properties button

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,16 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
 
+	$('td button').button({
+	  icons: {
+	    primary: "ui-icon-triangle-1-s"
+	  },
+	  text: false
+  })
+	.click(function() {
+		alert( "Display Edit Properties menu: Rename, Remove, New Property based on this Property..." )})
+							
+
 	$.datepicker.setDefaults({
 		dateFormat: 'mm/dd/yy',
 		changeMonth: true,

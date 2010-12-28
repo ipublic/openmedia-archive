1901c96b3c90efa954f1818d282cc4d103d33b6e
Dan Thomas <dan.thomas@ipublic.org>
2010-12-28 18:31:17 -0500

    added format_date and show_fields methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,5 +21,42 @@ module ApplicationHelper
   def filter_internal_properties(properties)
     properties.reject! { |k,v| ["_id", "_rev", "import_id", "couchrest-type", "created_at", "updated_at"].include?(k.to_s) }
   end
+  
+  def format_date(date_val)
+    if date_val.is_a?(Date) || date_val.is_a?(Time)
+      date_val.to_s(:full_date_only)
+    else
+      ""
+    end
+  end
+
+  def show_fields(object, field_list)
+    res = field_list.inject("<table class='propertyTable'>") do |html, a_field|
+      if a_field.is_a?(String)
+        html+="<tr>" + show_field(object, a_field) + "</tr>"
+      else
+        html+="<tr>" + show_field(object, * a_field) + "</tr>" 
+      end
+    end
+    res += "</table>"
+  end
+
+  def show_field (object, field_name, field_label=field_name.to_s.humanize)
+    value = object.fetch(field_name.to_s,'')
+    if value.is_a?(Integer)
+      "<th>#{field_label}:</th> <td>#{number_with_delimiter(value)}</td>"
+    elsif value.is_a?(Bignum)
+      "<th>#{field_label}:</th> <td>#{number_with_delimiter(value)}</td>"
+    elsif value.is_a?(Date)
+      "<th>#{field_label}:</th> <td>#{value.to_s(:full_date_only)}</td>"
+    elsif value.is_a?(Time)
+      "<th>#{field_label}:</th> <td>#{value.to_s(:full_date_only)}</td>"
+    elsif value.is_a?(ActiveRecord::Base)
+      "<th>#{field_label}:</th> <td>#{link_to h(value), value}</td>"
+    else
+      "<th>#{field_label}:</th> <td>#{h(value)}</td>"
+    end
+  end
+  
 
 end

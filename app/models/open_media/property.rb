1e6375a2631c5332d924ce9535c5a14d410651bd
Dan Thomas <dan.thomas@ipublic.org>
2010-12-30 17:28:13 -0500

    Removed is_key and query properties Added geometry_type to types constant array

--- app/models/open_media/property.rb
+++ app/models/open_media/property.rb
@@ -4,21 +4,22 @@ class OpenMedia::Property < Hash
   INTEGER_TYPE = 'integer'
   DATE_TYPE = 'date'
   DATETIME_TYPE = 'datetime'
+  GEOMETRY_TYPE = 'geometry'
 
-  TYPES = [STRING_TYPE, INTEGER_TYPE, DATE_TYPE, DATETIME_TYPE]
+#  TYPES = [STRING_TYPE, INTEGER_TYPE, DATE_TYPE, DATETIME_TYPE]
+  TYPES = [STRING_TYPE, INTEGER_TYPE, DATETIME_TYPE, GEOMETRY_TYPE]
   
   include CouchRest::Model::CastedModel
 
   property :name, :readonly=>true
+  property :namespace
   property :data_type
   property :display_name
   property :definition
   property :default_value
   property :example_value
-  property :can_query
-  property :is_key
-  property :example_value
   property :comment
   property :source_name
+  
 end
 

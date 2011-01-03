477f35063b720a63fe42abcc841a7474b241117c
Dan Thomas <dan.thomas@ipublic.org>
2011-01-03 16:46:49 -0500

    removed reference to non-existent Topic model

--- app/models/open_media/schema/enumerated_type.rb
+++ app/models/open_media/schema/enumerated_type.rb
@@ -2,7 +2,7 @@ class OpenMedia::Schema::EnumeratedType < OpenMedia::Schema::Type
 
   # An enumerated type refers to a set of Topics (instances)
 
-  property :topics, [OpenMedia::Schema::Topic]
+#  property :topics, [OpenMedia::Schema::Topic]
   #  collection_of :topics, :class_name=>'OpenMedia::Schema::Property'
 
 end

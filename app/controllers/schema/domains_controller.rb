6460148debab04648426a5baaf5a0b7350641681
Dan Thomas <dan.thomas@ipublic.org>
2011-01-06 21:49:10 -0500

    changed property name web_address to identifier updated domain model to support namespace property

--- app/controllers/schema/domains_controller.rb
+++ app/controllers/schema/domains_controller.rb
@@ -11,7 +11,7 @@ class Schema::DomainsController < ApplicationController
   
   def show
     @domain = OpenMedia::Schema::Domain.get(params[:id])
-    @types = OpenMedia::Schema::Type.find_by_domain_id(@domain.web_address)
+    @types = OpenMedia::Schema::Type.find_by_domain_id(@domain.identifier)
     @types ||= []
     
     respond_to do |format|
--- app/models/open_media/schema/domain.rb
+++ app/models/open_media/schema/domain.rb
@@ -1,37 +1,59 @@
 class OpenMedia::Schema::Domain < CouchRest::Model::Base
   
   use_database TYPES_DATABASE
-  unique_id :web_address
+  unique_id :identifier
   
+  property :namespace
   property :name
-  property :web_address
-  property :hidden
+  property :identifier
+  property :hidden, :default => false
   
   property :creator
   property :permission
   
   timestamps!
 
+  validates_presence_of :namespace
   validates :name, :presence => true, :uniqueness => true
-  validates :web_address, :presence => true, :uniqueness => true
+  validates :identifier, :presence => true, :uniqueness => true
 
   ## Views
+  view_by :namespace
   view_by :name
-  view_by :creator
   
   ## Callbacks
-  before_save :generate_identifier
+  before_save :create_database
+  
+  def namespace=(namespace)
+    self['namespace'] = namespace
+    generate_identifier
+  end
+  
+  def name=(name)
+    self['name'] = name
+    generate_identifier
+  end
 
   def type_count
-    domain_types = OpenMedia::Schema::Type.by_domain_id(:key => self.web_address)
+    domain_types = OpenMedia::Schema::Type.by_domain_id(:key => self.identifier)
     count = domain_types.nil? ? 0 : domain_types.count
   end
 
 private
 
   def generate_identifier
-#    self['web_address'] = '/' + self.database.name + '/' + self['web_address'] if new?
-    self['web_address'] = self['web_address'] if new?
+    return if self.namespace.nil? || self.namespace.empty?
+    return if self.name.nil? || self.name.empty?
+
+    # ID is form <namespace>_<name> where name is only lower case alpha & numeric characters
+    self.identifier = self.namespace + "_" + self.name.downcase.gsub(/[^a-z0-9]/,'').gsub(/^\-|\-$/,'')
+    create_database
   end
+  
+  def create_database
+#    COUCHDB_SERVER.database().create! 
+    COUCHDB_SERVER.define_available_database(self.identifier.to_sym, self.identifier) 
+  end
+
   
 end

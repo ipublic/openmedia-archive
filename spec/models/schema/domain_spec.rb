7294ae924ae8901db347780dd5f83cfc97b31961
Dan Thomas <dan.thomas@ipublic.org>
2011-01-06 21:02:26 -0500

    initial commit

---
+++ spec/models/schema/domain_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe OpenMedia::Schema::Domain do
+
+  before(:each) do
+    reset_test_db!
+    @domain = OpenMedia::Schema::Domain.new(:namespace => "om", :name => "Public Safety")
+  end
+  
+  it 'requires name to save' do
+    d2 = OpenMedia::Schema::Domain.new(:namespace => "om")
+    d2.save.should be_false
+    d2.should_not be_valid
+    d2.errors[:name].should_not be_nil
+  end
+
+  it 'requires namespace to save' do
+    d2 = OpenMedia::Schema::Domain.new(:name => "Public Safety")
+    d2.save.should be_false
+    d2.should_not be_valid
+    d2.errors[:name].should_not be_nil
+  end
+
+  it 'given a namespace and name, should generate an ID' do
+    @domain.identifier.should_not be_nil
+  end
+
+  it 'should require IDs to be unique' do
+    @domain.save
+    d2 = OpenMedia::Schema::Domain.new(:namespace => "om", :name => "Public Safety")
+    d2.save.should be_false
+    d2.should_not be_valid
+    d2.errors[:name].should_not be_nil
+  end
+
+  it 'should create a CouchDB database named after ID' do
+    @domain.save
+    COUCHDB_SERVER.available_database?(@domain.identifier).should be_true
+  end
+
+end

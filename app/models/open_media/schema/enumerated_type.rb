a4800827392bb9199bd6bd840aab2f1077ee6c24
Greg Lappen <greg@lapcominc.com>
2011-02-04 11:00:42 -0500

    removed old schema models and controllers

--- app/models/open_media/schema/enumerated_type.rb
+++
@@ -1,8 +0,0 @@
-class OpenMedia::Schema::EnumeratedType < OpenMedia::Schema::Type
-
-  # An enumerated type refers to a set of Topics (instances)
-
-#  property :topics, [OpenMedia::Schema::Topic]
-  #  collection_of :topics, :class_name=>'OpenMedia::Schema::Property'
-
-end

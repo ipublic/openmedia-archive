4d5f2c7179e803e1996d4564dda7aed8ba896ae3
Greg Lappen <greg@lapcominc.com>
2011-04-06 21:50:47 -0400

    added ipublic site setup including example skos collections to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,25 @@ OpenMedia::Schema::RDFS::Datatype.for(RDF::OM_CORE.GeoJson).save!
 require File.join(File.dirname(__FILE__),'seeds', 'metadata')
 require File.join(File.dirname(__FILE__),'seeds', 'vcard')
 TYPES_RDF_REPOSITORY.refresh_design_doc   # just in case
+
+OpenMedia::Schema::VCard.initialize_vcard
+OpenMedia::Schema::Metadata.initialize_metadata
+
+# create ipublic site w/ dan as admin
+ec = OpenMedia::InferenceRules::GeographicName.find_by_name('Ellicott City').first
+ipublic_site = OpenMedia::Site.create!(:url=>"http://ipublic.#{DOMAIN}", :municipality=>ec, :openmedia_name=>'iPublic OpenMedia Portal')
+ipublic_site.initialize_metadata
+
+dan = Admin.create!(:email=>'dan.thomas@ipublic.org', :password=>'ChangeMe',
+                    :password_confirmation=>'ChangeMe', :site=>ipublic_site, :confirmed_at=>Time.now)
+
+name = OpenMedia::Schema::OWL::Class::HttpDataCivicopenmediaOrgCoreVcardName.new(:given_name=>'Dan', :family_name=>'Thomas').save!
+email = OpenMedia::Schema::OWL::Class::HttpDataCivicopenmediaOrgCoreVcardEmail.new(:value=>'dan.thomas@ipublic.org', :type=>RDF::VCARD.Work.to_s).save!
+vcard = OpenMedia::Schema::OWL::Class::HttpDataCivicopenmediaOrgCoreVcardVcard.for(ipublic_site.vcards_rdf_uri/UUID.new.generate.gsub(/-/,''))
+vcard.n = name
+vcard.email = email
+vcard.save!
+
+OpenMedia::Schema::SKOS::Collection.create_in_collection!(ipublic_site.skos_collection, :label=>'Public Safety')
+OpenMedia::Schema::SKOS::Collection.create_in_collection!(ipublic_site.skos_collection, :label=>'Environmental')
+OpenMedia::Schema::SKOS::Collection.create_in_collection!(ipublic_site.skos_collection, :label=>'Recreation')

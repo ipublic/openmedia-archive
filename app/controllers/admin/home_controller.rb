ab6849ef328ffbaaea83b0ed9a1f98d508c7892f
Dan Thomas <dan.thomas@ipublic.org>
2011-04-08 10:55:05 -0400

    added code to lookup site collections

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,4 +1,20 @@
 class Admin::HomeController < ApplicationController
   before_filter :authenticate_admin!
+
+  def index
+    @collections = ipublic_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label}
+    if current_site
+      @collections.concat(current_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label})
+    end
+  end
+
+  def show
+    @collection = OpenMedia::Schema::SKOS::Collection.for(params[:id])
+    if current_site
+      @classes = @collection.concepts.collect {|c| c.rdfs_class}.select{|c| current_site.skos_collection.uri.parent==c.uri.parent.parent}
+    else
+      @classes = @collection.concepts.collect {|c| c.rdfs_class}
+    end
+  end
   
 end

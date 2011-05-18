a821c5da35e820a64a6a768df06daa0577ce03bc
Dan Thomas <dan.thomas@ipublic.org>
2011-05-18 09:07:45 -0400

    Base code to show map markers.  Only first marker shows.

--- app/controllers/admin/maps_controller.rb
+++ app/controllers/admin/maps_controller.rb
@@ -4,15 +4,60 @@ class Admin::MapsController < ApplicationController
   def index
     @maps = OpenMedia::Map.all
   end
+  
+  def feature_ajax
+    render :text => "Success"
+  end
 
   def show
     @map = OpenMedia::Map.get(params[:id])
     
     # Load GoogleMaps
-      initialize_map
-      # @map.zoom = 10
-      # @map.center = @site.ll
-      # @map.markers << @site.geomarker
+    initialize_gmap
+    @gmap.zoom = 11
+      
+    # Generate the Map Pins
+    @recs = OpenMedia::Schema.get_records(@map.feature_class)
+
+    p = OpenMedia::Site.first.municipality.geometries.first
+    pos = p['coordinates'].reverse if p['type'] == "Point"
+    @gmap.markers << Cartographer::Gmarker.new(:position => pos)
+    
+    @markers = []
+    @recs.each do |r|
+      pos = r['geometry']['coordinates'].reverse if r['geometry']['type'] == "Point"
+      mrk = Cartographer::Gmarker.new(
+                                                 # :icon => @icon,
+                                                 # :marker_type => "Organization", 
+                                                 :position => pos)
+      @markers << mrk
+    end
+    @gmap.markers = @markers
+    # 
+#    @recs = OpenMedia::Site.first.to_a # OpenMedia::Schema.get_records(@map.feature_class)
+
+    # r = OpenMedia::Site.first.municipality.geometries.first
+    # 
+    # pos = r['coordinates'].reverse if r['type'] == "Point"
+#    prop_link = link_text = 'Show on map',, options = {})
+#    prop_html = [{:title => "tab1", :html => "<p>1</p>"}, {:title => "tab2", :html => "<p>2</p>"}]
+#     prop_html = "<p>1</p>"
+#     prop_link = "/some_link"
+#     iwin = Cartographer::InfoWindow.new(:content => '<p>You <i>clicked</i> my marker</p>')
+#     
+#     @icon = Cartographer::Gicon.new()
+#     @gmap.icons << @icon
+# #    @gmap.marker_mgr = true
+# 
+#     @gmap.markers << Cartographer::Gmarker.new(
+#                                                :marker_type => "Organization",
+#                                                :icon => @icon,
+#                                                :info_window => '<p>here is the content</p>',
+# #                                               :info_window_url => prop_link,
+# #                                               :info_window => '<p>You <i>clicked</i> my marker</p>'.html_safe,
+#                                                :position => pos)
+#     
+#     
   end
 
   def new

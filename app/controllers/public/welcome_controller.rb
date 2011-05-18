fbc204ebd7e3c4c12574f5ddb2f79cec2b8cc621
Dan Thomas <dan.thomas@ipublic.org>
2011-05-18 09:03:38 -0400

    Add site branding and google maps

--- app/controllers/public/welcome_controller.rb
+++ app/controllers/public/welcome_controller.rb
@@ -1,10 +1,18 @@
 class Public::WelcomeController < ApplicationController
   layout 'public'
+  include GoogleMapsHelper
 
   def index
-    # Determine which OM Site
-    @site = OpenMedia::Site.first
-    @site_banner = @site.welcome_message
+    @site = current_site
+    @site = om_site if @site.nil?
+
+    @collections = om_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label}
+    if current_site
+      @collections.concat(current_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label}) unless current_site == om_site
+
+      # Load GoogleMaps
+      initialize_gmap
+    end
   end
 
   def license

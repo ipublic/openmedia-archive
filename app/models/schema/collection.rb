28dcb6e80faa860b613c6ea5c2c0060fb5506e4f
Dan Thomas <dan.thomas@ipublic.org>
2011-05-27 07:08:22 -0400

    Added views

--- app/models/schema/collection.rb
+++ app/models/schema/collection.rb
@@ -1,10 +1,42 @@
 class Schema::Collection < CouchRest::Model::Base
-  
+
+  use_database TYPES_DATABASE
+  unique_id :identifier
+
+  property :identifier
   property :namespace, Schema::Namespace
+  property :comment, String    # RDFS#Comment
 
   property :label, String       # User assigned name, RDFS#Label
   property :tags, [String]
 
   timestamps!
+
+  validates_presence_of :label
+  validates_uniqueness_of :identifier, :view => 'all'
+
+  ## Callbacks
+#  before_save :generate_identifier
+  before_create :generate_identifier
+
+  view_by :label
+  view_by :tags,
+    :map =>
+      "function(doc) {
+        if (doc['couchrest-type'] == 'Schema::Collection' && doc.tags) {
+          doc.tags.forEach(function(tag){
+            emit(doc.tag, 1);
+          });
+        }
+      }",
+    :reduce =>
+      "function(keys, values, rereduce) {
+        return sum(values);
+      }"
+
+private
+  def generate_identifier
+    self['identifier'] = self.class.to_s.downcase + '_' + label.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
+  end
 
 end

019e6e5c3c527788ab4e69469c1acca55a1e0535
Dan Thomas <dan.thomas@ipublic.org>
2011-05-28 09:46:41 -0400

    Renamed to property.rb

--- app/models/schema/vocabulary_class.rb
+++
@@ -1,9 +0,0 @@
-class Schema::VocabularyClass < Hash
-  include CouchRest::Model::CastedModel
-
-  property :label, String       # User assigned name, RDFS#Label
-  property :commment, String    # RDFS#Comment
-
-  property :terms, [Schema::PropertyDefinition]
-
-end

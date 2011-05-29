5cc2583acc95579162b2193bbdd52e4caa294479
Dan Thomas <dan.thomas@ipublic.org>
2011-05-28 21:36:37 -0400

    Added alias formatting and validations

--- app/models/schema/namespace.rb
+++ app/models/schema/namespace.rb
@@ -3,5 +3,10 @@ class Schema::Namespace < Hash
   
   property :alias, String            # alias => "foaf"
   property :iri_base, String         # iri_base => "http://xmlns.com/foaf/0.1/"
-  
+
+  validates_presence_of :alias, :iri_base
+
+  def alias=(value)
+    self["alias"] = value.to_s.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'')
+  end
 end

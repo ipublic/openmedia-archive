52ed8345fcdc6c3f9b54df0e1175b425e3468f72
Dan Thomas <dan.thomas@ipublic.org>
2011-05-30 19:18:45 -0400

    Updated to use Namespace as CouchRest::Model

--- app/models/schema/collection.rb
+++ app/models/schema/collection.rb
@@ -2,22 +2,24 @@ class Schema::Collection < CouchRest::Model::Base
 
   use_database TYPES_DATABASE
   unique_id :identifier
+  
+  belongs_to :namespace, :class_name => "Schema::Namespace"
 
   property :identifier, String
   property :label, String       # User assigned name, RDFS#Label
   property :comment, String     # RDFS#Comment
-  property :namespace, Schema::Namespace
   property :tags, [String]
 
   timestamps!
 
-  validates_presence_of :label, :namespace
+  validates_presence_of :label, :namespace_id
   validates_uniqueness_of :identifier, :view => 'all'
 
   ## Callbacks
   before_create :generate_identifier
 
   view_by :label
+  view_by :namespace_id
   
   view_by :tags,
     :map =>
@@ -28,24 +30,19 @@ class Schema::Collection < CouchRest::Model::Base
             });
           }
         }"
-      
-  view_by :iri_base,
-    :map => 
-      "function(doc) {
-        if ((doc['couchrest-type'] == 'Schema::Collection') && (doc.namespace['iri_base'] != null)) { 
-            emit(doc.namespace['iri_base'], null);
-            }
-        }"
 
-
-  def vocabularies
-    Schema::Vocabulary.by_collection_id(:key => self.identifier)
+  ##
+  # Returns the Collections for passed Namespace.
+  #
+  # @return [Collection]  
+  def self.find_by_namespace_id(ns_id)
+    self.by_namespace_id(:key => ns_id)
   end
-
+      
 private
   def generate_identifier
     self['identifier'] = self.class.to_s.split("::").last.downcase + '_' +
-                         self.namespace.alias.to_s.downcase + '_' +
+                         self.namespace.authority_uri + '_' +
                          label.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
   end
 

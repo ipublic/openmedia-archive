e8de388b536a6811ad7c94f6b92bfd38a1683fe9
Dan Thomas <dan.thomas@ipublic.org>
2011-06-05 19:37:40 -0400

    Non-couchdb hash version

--- app/models/om_linked_data/namespace.rb
+++ app/models/om_linked_data/namespace.rb
@@ -1,21 +1,24 @@
-class OmLinkedData::Namespace < Hash
+class OmLinkedData::Namespace
   require "addressable/uri"
-  include CouchRest::Model::CastedModel
-  
-  property :uri
-  property :fqdn
-  property :subdomain
-  property :authority
-  
-  # Expects a URI with subdomain, e.g.; http://om.civicopenmedia.us
-  def initialize(uri)
-    self.uri = uri
 
-    addr = Addressable::URI.parse(self.uri)
-    self.fqdn = addr.host
-    self.authority = escape_string(self.fqdn)
+  attr_reader :fqdn, :authority, :subdomain, :base_uri
+
+  # Expects a URI with subdomain, e.g.; http://om.civicopenmedia.us
+  def initialize(base_uri)
+    @base_uri = base_uri.to_s
+    addr = Addressable::URI.parse(@base_uri)
+    @fqdn = addr.host
+    @authority = escape_string(@fqdn)
     parts = addr.host.split('.')
-    self.subdomain = parts.first if parts.length == 3
+    @subdomain = parts.first if parts.length == 3
+    to_hash
+  end
+
+  def to_hash
+    Hash[:base_uri => @base_uri,
+         :fqdn => @fqdn,
+         :authority => @authority,
+         :subdomain => @subdomain]
   end
   
 private

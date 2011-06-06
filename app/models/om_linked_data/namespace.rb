64fcd4e3c9040e1667656d548222f6b4699afefd
Dan Thomas <dan.thomas@ipublic.org>
2011-06-05 20:06:08 -0400

    CastedModel version

--- app/models/om_linked_data/namespace.rb
+++ app/models/om_linked_data/namespace.rb
@@ -1,26 +1,22 @@
-class OmLinkedData::Namespace
+class OmLinkedData::Namespace < Hash
+  include CouchRest::Model::CastedModel
   require "addressable/uri"
 
-  attr_reader :fqdn, :authority, :subdomain, :base_uri
-
-  # Expects a URI with subdomain, e.g.; http://om.civicopenmedia.us
-  def initialize(base_uri)
-    @base_uri = base_uri.to_s
-    addr = Addressable::URI.parse(@base_uri)
-    @fqdn = addr.host
-    @authority = escape_string(@fqdn)
-    parts = addr.host.split('.')
-    @subdomain = parts.first if parts.length == 3
-    to_hash
-  end
-
-  def to_hash
-    Hash[:base_uri => @base_uri,
-         :fqdn => @fqdn,
-         :authority => @authority,
-         :subdomain => @subdomain]
-  end
+  property :base_uri
+  property :fqdn
+  property :authority
+  property :subdomain
   
+  def initialize(b)
+    super
+    self.base_uri = b.to_s
+    addr = Addressable::URI.parse(self.base_uri)
+    self.fqdn = addr.host
+    self.authority = escape_string(self.fqdn)
+    parts = addr.host.split('.')
+    self.subdomain = parts.first if parts.length == 3
+  end
+
 private
   def escape_string(str)
     str.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') 

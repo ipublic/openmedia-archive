a6040d8bd787f40961b4081d50dbe91a36d3a99f
Dan Thomas <dan.thomas@ipublic.org>
2011-06-23 13:00:46 -0400

    Fix: implement new model design

--- app/models/om_linked_data/property.rb
+++ app/models/om_linked_data/property.rb
@@ -3,39 +3,36 @@ class OmLinkedData::Property < CouchRest::Model::Base
   use_database TYPES_DATABASE
   unique_id :identifier
 
-  property :identifier, :init_method => 'new'
+  belongs_to :vocabulary, :class_name => "OmLinkedData::Vocabulary"
+  collection_of :synonyms, :class_name => 'OmLinkedData::Property'  # Props with same semantic meaning
+
+  property :identifier
   property :label, String                             # User assigned name, RDFS#Label
   property :comment, String                           # => RDFS#Comment
   property :tags, [String]
 
-  property :base_uri, String
-  property :subdomain, String
-  property :uri, String
   property :term, String                              # Escaped vocabulary name suitable for inclusion in IRI
-  property :authority, String
+  property :uri, String
 
-#  property :range                                     # => RDFS.range
-  property :expected_type, OmLinkedData::Type     # Support type coercion
+  property :expected_type, OmLinkedData::Type         # Support type coercion
   
-  property :deprecated, TrueClass, :default => false  # Deprecated propoerties may not be used 
-                                                       # for future vocabularies
-                                                      
-  property :enumerations                               # Hash of key/value lookups
-  
-  collection_of :synonyms, :class_name => 'OmLinkedData::Property'  # Props with same semantic meaning
-  
+  property :enumerations                              # Hash of key/value lookups
+  property :deprecated, TrueClass, :default => false  # Deprecated properties may not be used 
+                                                       # for future vocabularies            
   timestamps!
 
   validates_presence_of :label
-  validates_presence_of :base_uri
+  # validates_presence_of :expected_type
+  validates_presence_of :vocabulary_id
   validates_uniqueness_of :identifier, :view => 'all'
   
   before_create :generate_uri
   before_create :generate_identifier
   
+  view_by :uri
   view_by :label
-  view_by :authority
-  view_by :base_uri
+  view_by :term
+  view_by :vocabulary_id
 
   view_by :tags,
     :map =>
@@ -47,34 +44,30 @@ class OmLinkedData::Property < CouchRest::Model::Base
           }
         }"
 
-  # Property Name in Compact URI form => "foaf:name"
-  def curie
-    self.namespace['subdomain'] + ':' + self.term
+  def deprecated?
+    self.deprecated
+  end
+
+  def self.find_by_vocabulary_id(vocab_id)
+    self.by_vocabulary_id(:key => vocab_id)
   end
   
 private
   def generate_uri
-    
-    # base_uri => "http://dcgov.civicopenmedia.us"
-    parts = base_uri.split('.')
-    self.subdomain = parts.first.split("http://").last
-    domain = base_uri.split(subdomain + '.').last.gsub('.','_')
-
-    # authority => "civicopenmedia_us_dcgov"
-    self.authority = domain + '_' + subdomain
-    self.term = escape_string(self.label)
-
-    rdf_uri = RDF::URI.new('http://civicopenmedia.us')/self.subdomain/"terms#"/self.term
+    self.term ||= escape_string(self.label)
+    rdf_uri = RDF::URI.new(self.vocabulary.uri)/self.vocabulary.property_delimiter + self.term
     self.uri = rdf_uri.to_s
   end
 
   def generate_identifier
     self['identifier'] = self.class.to_s.split("::").last.downcase + '_' +
-                         self.authority + '_' + self.term if new?
+                         self.vocabulary.authority + '_' + 
+                         self.vocabulary.term.downcase + '_' +
+                         escape_string(self.term.downcase) if new?
   end
 
   def escape_string(str)
-    str.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') 
+    str.gsub(/[^A-Za-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') 
   end
 
 end

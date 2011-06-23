426614aa4dae06a4cb2db76c5c9d879514e4b11a
Dan Thomas <dan.thomas@ipublic.org>
2011-06-23 09:26:40 -0400

    Fix: Dashboard errors when no site subdomain

--- app/controllers/public/welcome_controller.rb
+++ app/controllers/public/welcome_controller.rb
@@ -6,17 +6,16 @@ class Public::WelcomeController < ApplicationController
     @site = current_site
     @site = om_site if @site.nil?
     
-    dash_id = current_site.default_dashboard
-    if dash_id.blank?
-      @dashboard = OpenMedia::Dashboard.first
-    else
-      @dashboard = OpenMedia::Dashboard.get dash_id
-    end
-    
-
     @collections = om_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label}
     if current_site
       @collections.concat(current_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label}) unless current_site == om_site
+
+      dash_id = current_site.default_dashboard
+      if dash_id.blank?
+        @dashboard = OpenMedia::Dashboard.first
+      else
+        @dashboard = OpenMedia::Dashboard.get dash_id
+      end
 
       # Load GoogleMaps
       initialize_gmap

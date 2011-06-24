479a0cf3cd3e5c776401119e4d4c65daeb5d0d4f
Dan Thomas <dan.thomas@ipublic.org>
2011-06-24 11:57:29 -0400

    Fixed: return collections from couch model

--- app/controllers/public/welcome_controller.rb
+++ app/controllers/public/welcome_controller.rb
@@ -6,9 +6,12 @@ class Public::WelcomeController < ApplicationController
     @site = current_site
     @site = om_site if @site.nil?
     
-    @collections = om_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label}
+    ns = OmLinkedData::Namespace.new @site.url
+    @collections = ::OmLinkedData::Collection.by_base_uri(:key => ns.base_uri)
+    
+    # @collections = om_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label}
     if current_site
-      @collections.concat(current_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label}) unless current_site == om_site
+      # @collections.concat(current_site.skos_collection.sub_collections.sort{|c1,c2| c1.label <=> c2.label}) unless current_site == om_site
 
       dash_id = current_site.default_dashboard
       if dash_id.blank?

bc5851a999a822196dc14bf12e54480006f8f58b
Dan Thomas <dan.thomas@ipublic.org>
2011-06-25 13:48:08 -0400

    Add: geojson#point type

--- db/seeds/vocabularies/municipality.rb
+++ db/seeds/vocabularies/municipality.rb
@@ -15,26 +15,28 @@ vocab = ::OmLinkedData::Vocabulary.new(:base_uri => "http://civicopenmedia.us/vo
                                         :tags => tags
                                         ).save
 
-prop_name = ::OmLinkedData::Property.create!(:vocabulary => vocab, 
+name_prop = ::OmLinkedData::Property.create!(:vocabulary => vocab, 
                                           :label => "Name", 
                                           :term => "name", 
                                           :expected_type => xsd_string,
                                           :comment => "Official place name"
                                           )                              
 
-prop_source = ::OmLinkedData::Property.create!(:vocabulary => vocab, 
+source_prop = ::OmLinkedData::Property.create!(:vocabulary => vocab, 
                                           :label => "Source",
                                           :term => "source", 
                                           :expected_type => xsd_string,
                                           :comment => "The URL or other identifier of authority where municipal name was obtained"
                                           )                              
 
-prop_source_id = ::OmLinkedData::Property.create!(:vocabulary => vocab, 
+source_prop_id = ::OmLinkedData::Property.create!(:vocabulary => vocab, 
                                           :label => "Source ID",
                                           :term => "source_id", 
                                           :expected_type => xsd_string,
                                           :comment => "The Source authority's URI or other key for municipal name"
                                           )                              
 
-vocab.properties << prop_name << prop_source << prop_source_id
+geojson_point_type = OmLinkedData::Type.find_by_uri(:key => "http://civicopenmedia.us/vocabularies/GeoJSON#Point")
+
+vocab.properties << name_prop << source_prop << source_prop_id << geojson_point_type
 vocab.save!

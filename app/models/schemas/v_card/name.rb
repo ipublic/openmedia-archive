1dbe4fa0319483302347262a9340cca81dadcf49
Dan Thomas <dan.thomas@ipublic.org>
2011-07-10 13:34:15 -0400

    Fix: assign top-level properties to VCard class

--- app/models/schemas/v_card/name.rb
+++ app/models/schemas/v_card/name.rb
@@ -2,13 +2,10 @@ class Schemas::VCard::Name < Hash
   include CouchRest::Model::CastedModel    
 
   # based on vcard properties
-  property :full_name, String     # :alias => :fn
   property :first_name, String    # :alias => "given-name"
   property :middle_name, String   # :alias => "additional-name"
   property :last_name, String     # :alias => :family-name
-  property :nickname, String      # :alias => :nickname
   property :prefix, String        # :alias => "honorific-prefix"
   property :suffix, String        # :alias => "honorific-suffix"
-  property :job_title, String     # :alias => "Title"
 
 end

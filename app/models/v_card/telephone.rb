e9f09a656890331eaba516839a5ad6499ddb7b28
Dan Thomas <dan.thomas@ipublic.org>
2011-07-15 18:24:36 -0500

    Add: constant for telephone type

--- app/models/v_card/telephone.rb
+++ app/models/v_card/telephone.rb
@@ -1,6 +1,8 @@
 class VCard::Telephone < Hash
   include CouchRest::Model::CastedModel    
 
+  TELEPHONE_TYPES = %w(BBS Car Cell Fax Home ISDN Modem Msg PCS Pager Video Voice Work Pref)
+
   # based on vcard properties
   property :type   # BBS, Car, Cell, Fax, Home, ISDN, Modem, Msg, PCS, Pager, Video, Voice, Work, Pref
   property :value

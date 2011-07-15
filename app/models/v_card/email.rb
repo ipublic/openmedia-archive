53539a45d64fe7b52d959f2a5828cd364167f7e0
Dan Thomas <dan.thomas@ipublic.org>
2011-07-15 18:24:03 -0500

    Add: constant of email type

--- app/models/v_card/email.rb
+++ app/models/v_card/email.rb
@@ -1,6 +1,8 @@
 class VCard::Email < Hash
   include CouchRest::Model::CastedModel    
 
+  EMAIL_TYPES = %w(Home Work)
+
   # based on vcard properties
   property :type   # Home, Work
   property :value

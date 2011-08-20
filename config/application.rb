754f09ff791f0f335a306391a980ad0b62b1a97d
Dan Thomas <dan.thomas@ipublic.org>
2011-08-20 08:25:56 -0400

    Add: load version.rb

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require "action_mailer/railtie"
 require "active_resource/railtie"
 require "rails/test_unit/railtie"
 
+require "lib/version.rb"
+
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)

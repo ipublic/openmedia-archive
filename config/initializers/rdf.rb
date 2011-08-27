9c8ca2283868b76736ca4a4d7a4c366e9c28dddf
Dan Thomas <dan.thomas@ipublic.org>
2011-08-27 10:59:11 -0400

    Fix: disable initialization

--- config/initializers/rdf.rb
+++ config/initializers/rdf.rb
@@ -1,47 +1,47 @@
-require 'linkeddata'
-require 'rdf/couchdb'
-require 'spira'
-require 'open_media/schema'
-
-module Spira
- def settings
-   $SPIRA_SETTINGS ||= {}
- end
- module_function :settings
-
- class Errors
-   def count
-     @errors.size
-   end
-
-   def full_messages
-     messages = []
-     @errors.each {|k,v| messages << v}
-     messages.flatten!
-   end
-
-   def [](field)
-     @errors[field.to_sym] || []
-   end
- end
-end
-
-TYPES_RDF_REPOSITORY = RDF::CouchDB::Repository.new(:database=>TYPES_DATABASE)
-Spira.add_repository! 'types', TYPES_RDF_REPOSITORY
-
-begin
-  OpenMedia::Schema::DesignDoc.refresh
-
-  OpenMedia::Schema::RDFS::Class.for(::RDF::METADATA.Metadata)
-  OpenMedia::Schema::OWL::Class.for(::RDF::VCARD.VCard)
-
-  # setup vcard models
-  OpenMedia::Schema::VCard.initialize_vcard
-
-  
-  # setup metadata model
-  OpenMedia::Schema::Metadata.initialize_metadata
-rescue => e
-  puts e.inspect
-end
-
+# require 'linkeddata'
+# require 'rdf/couchdb'
+# require 'spira'
+# require 'open_media/schema'
+# 
+# module Spira
+#  def settings
+#    $SPIRA_SETTINGS ||= {}
+#  end
+#  module_function :settings
+# 
+#  class Errors
+#    def count
+#      @errors.size
+#    end
+# 
+#    def full_messages
+#      messages = []
+#      @errors.each {|k,v| messages << v}
+#      messages.flatten!
+#    end
+# 
+#    def [](field)
+#      @errors[field.to_sym] || []
+#    end
+#  end
+# end
+# 
+# TYPES_RDF_REPOSITORY = RDF::CouchDB::Repository.new(:database=>TYPES_DATABASE)
+# Spira.add_repository! 'types', TYPES_RDF_REPOSITORY
+# 
+# begin
+#   OpenMedia::Schema::DesignDoc.refresh
+# 
+#   OpenMedia::Schema::RDFS::Class.for(::RDF::METADATA.Metadata)
+#   OpenMedia::Schema::OWL::Class.for(::RDF::VCARD.VCard)
+# 
+#   # setup vcard models
+#   OpenMedia::Schema::VCard.initialize_vcard
+# 
+#   
+#   # setup metadata model
+#   OpenMedia::Schema::Metadata.initialize_metadata
+# rescue => e
+#   puts e.inspect
+# end
+# 

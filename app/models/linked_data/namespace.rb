053ed635bc3915b637498b29892c5020793436b1
Dan Thomas <dan.thomas@ipublic.org>
2011-09-01 19:16:24 -0400

    Fix: cast input argument to_s and return results as hash

--- app/models/linked_data/namespace.rb
+++ app/models/linked_data/namespace.rb
@@ -1,20 +1,16 @@
 class LinkedData::Namespace
-  
+
   attr_reader :base_uri, :authority, :domain, :subdomain
-  
+
   def initialize(openmedia_url)
-    parts = openmedia_url.split('.')
+    parts = openmedia_url.to_s.split('.')
     @subdomain = parts.first.split("http://").last
-    @domain = openmedia_url.split(subdomain + '.').last
+    @domain = openmedia_url.to_s.split(subdomain + '.').last
 
     # authority => "civicopenmedia_us_dcgov"
     @authority = domain.gsub('.','_') + '_' + subdomain
     @base_uri = "http://#{@domain}/#{@subdomain}"
+    Hash[:base_uri => @base_uri, :authority => @authority, :domain => @domain, :subdomain => @subdomain]
   end
 
-private
-  def escape_string(str)
-    str.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') 
-  end
-  
 end

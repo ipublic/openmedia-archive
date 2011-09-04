f8868d673bd0928bad92a200a398cac89a936dbf
Dan Thomas <dan.thomas@ipublic.org>
2011-09-04 13:43:09 -0400

    Fix: changed unique_id to URI. Updated tag_list custom view to query on 'model' property

--- app/models/linked_data/type.rb
+++ app/models/linked_data/type.rb
@@ -2,50 +2,45 @@ require 'rdf/couchdb'
 class LinkedData::Type < CouchRest::Model::Base
   
   use_database TYPES_DATABASE
-  unique_id :identifier
+  unique_id :uri
 
   belongs_to :vocabulary, :class_name => "LinkedData::Vocabulary"
 
   # Properties denote a HAS A relationship. For example, iPublic Has a url (property) of
   # http://ipublic.org (value)
-  collection_of :properties, :class_name => 'LinkedData::Property' 
+  property :properties, [LinkedData::Property]
   
   # Included types support set-based inclusion for building atop existing vocabulary definitions and to support 
   # inferencing.  For example, a resident is always a person, designating someone as a resident will 
   # provide access both to resident and person properties. Note: included properties aren't inheritance
   collection_of :included_types, :class_name => 'LinkedData::Type'
   
-  property :identifier
+  property :uri, String
+  property :term, String          # type id appended to URI.
   property :label, String         # User assigned name, RDFS#Label
   property :comment, String       # => RDFS#Comment
   property :tags, [String]
 
-  property :term, String          # type id appended to URI. preserves string case if provided, 
-                                  #   adapted from "label" property if not provided
-  property :uri, String
   property :enumerations           # Hash of key/value lookups
-
 
   timestamps!
 
   validates_presence_of :term
   validates_presence_of :vocabulary_id
-  validates_uniqueness_of :identifier, :view => 'all'
+  validates_uniqueness_of :uri, :view => 'all'
 
   ## Callbacks
   before_create :generate_uri
-  before_create :generate_identifier
 
   design do
-    view :by_label
     view :by_term
-    view :by_uri
+    view :by_label
     view :by_vocabulary_id
     
     view :tag_list,
       :map =>
         "function(doc) {
-          if (doc['type'] == 'LinkedData::Type' && doc.tags) {
+          if (doc['model'] == 'LinkedData::Type' && doc.tags) {
             doc.tags.forEach(function(tag) {
               emit(tag, 1); 
               });
@@ -55,22 +50,13 @@ class LinkedData::Type < CouchRest::Model::Base
     view :included_type_ids,
       :map =>
         "function(doc) {
-          if (doc['type'] == 'LinkedData::Type' && doc.included_type_ids) {
+          if (doc['model'] == 'LinkedData::Type' && doc.included_type_ids) {
             doc.included_type_ids.forEach(function(included_type_id) {
               emit(included_type_id, 1); 
               });
             }
           }"
 
-    view :property_ids,
-      :map =>
-        "function(doc) {
-          if (doc['type'] == 'LinkedData::Type' && doc.property_ids) {
-            doc.property_ids.forEach(function(property_id) {
-              emit(property_id, 1); 
-              });
-            }
-          }"
   end
 
   def compound?
@@ -87,17 +73,5 @@ private
     rdf_uri = RDF::URI.new(self.vocabulary.uri)/self.vocabulary.property_delimiter + self.term
     self.uri = rdf_uri.to_s
   end
-
-  def generate_identifier
-    self['identifier'] = self.class.to_s.split("::").last.downcase + '_' +
-                         self.vocabulary.authority + '_' + 
-                         self.vocabulary.term.downcase + '_' +
-                         escape_string(self.term.downcase) if new?
-  end
-
-  def escape_string(str)
-    str.gsub(/[^A-Za-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') 
-  end
-
 
 end
--- spec/models/linked_data/type_spec.rb
+++ spec/models/linked_data/type_spec.rb
@@ -29,6 +29,8 @@ describe LinkedData::Type do
                                           )
 
     @str = LinkedData::Type.create!(:vocabulary => @xsd, :term => "string")
+    @str_uri = 'http://www.w3.org/2001/XMLSchema#string'
+    @int_uri = 'http://www.w3.org/2001/XMLSchema#integer'
 
   end
   
@@ -36,6 +38,7 @@ describe LinkedData::Type do
     lambda { LinkedData::Type.create!() }.should raise_error
     lambda { LinkedData::Type.create!(:term => "integer") }.should raise_error
     lambda { LinkedData::Type.create!(:vocabulary => @crime) }.should raise_error
+    lambda { LinkedData::Type.create!(:vocabulary => @crime, :term => "integer") }.should_not raise_error
   end
 
   it 'should save and generate an identifier correctly' do
@@ -43,26 +46,16 @@ describe LinkedData::Type do
     int = LinkedData::Type.new(:vocabulary => @xsd, :term => term)
 
     lambda { int.save! }.should change(LinkedData::Type, :count).by(1)
-    @res = LinkedData::Type.by_term(:key => term)
-    @res.rows[0].id.should == 'type_civicopenmedia_us_dcgov_xmlschema_integer'
-  end
-  
-  it 'should return this Type when searching by URI' do
-    @res = LinkedData::Type.create!(:vocabulary => @xsd, :term => "integer")
-
-    @res.uri.should == "http://www.w3.org/2001/XMLSchema#integer"
-    @types = LinkedData::Type.by_uri(:key => @res.uri)
-    @types.length.should == 1
-    @types.rows[0].id.should == "type_civicopenmedia_us_dcgov_xmlschema_integer"
+    @res = LinkedData::Type.get(@int_uri)
+    @res.id.should == @int_uri
   end
   
   it 'should return this Type when searching by Vocabulary' do
     term = "integer"
     int = LinkedData::Type.create!(:vocabulary => @xsd, :term => term)
 
-    int.save!
-    @types = LinkedData::Type.find_by_vocabulary_id(@xsd.identifier)
-    @types.rows[0].id.should == "type_civicopenmedia_us_dcgov_xmlschema_integer"
+    @types = LinkedData::Type.find_by_vocabulary_id(@xsd.id)
+    @types.rows[0].id.should == @int_uri
   end
   
   it 'should use tags view to return matching docs' do
@@ -73,7 +66,7 @@ describe LinkedData::Type do
     @res = LinkedData::Type.tag_list(:key => "xyxyxy")
     @res.length.should == 0 
     @res = LinkedData::Type.tag_list(:key => "intrinsic")
-    @res.rows[0].id.should == 'type_civicopenmedia_us_dcgov_xmlschema_integer'
+    @res.rows[0].id.should == @int_uri
   end
   
   it 'should save and return an external vocabulary and Type' do
@@ -84,17 +77,12 @@ describe LinkedData::Type do
   
   it 'should save and return a Compound Type' do 
     cr = LinkedData::Type.new(:vocabulary => @crime, 
-                                :label => "Crime Reports",
-                                :term => "crime_reports"
-                                )
+                              :label => "Crime Reports",
+                              :term => "crime_reports"
+                              )
     
-    method = LinkedData::Property.create!(:vocabulary => @crime, 
-                                         :term => "Method", 
-                                         :expected_type => @str)
-    offense = LinkedData::Property.create!(:vocabulary => @crime, 
-                                         :term => "Offense", 
-                                         :expected_type => @str)
-
+    method = LinkedData::Property.new(:term => "Method", :expected_type => @str.uri)
+    offense = LinkedData::Property.new(:term => "Offense", :expected_type => @str.uri)
                                      
     cr.properties << method << offense
     comp = cr.save
@@ -102,7 +90,7 @@ describe LinkedData::Type do
     res = LinkedData::Type.get(comp.id)
     
     res.properties[0].term.should == "Method"
-    res.properties[0].expected_type.label.should == "string"
+    res.properties[0].expected_type.should == @str_uri
     res.compound?.should == true
   end
   

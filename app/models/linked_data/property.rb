9cbb1aa01b1a98a737a2f8cf909843b86fcd429d
Dan Thomas <dan.thomas@ipublic.org>
2011-09-04 10:25:51 -0400

    Fix: changed to CouchRest::Model::Embeddable

--- app/models/linked_data/property.rb
+++ app/models/linked_data/property.rb
@@ -1,73 +1,21 @@
-require 'rdf/couchdb'
-class LinkedData::Property < CouchRest::Model::Base
-
-  use_database TYPES_DATABASE
-  unique_id :identifier
-
-  property :identifier
-  property :label, String                             # User assigned name, RDFS#Label
-  property :comment, String                           # => RDFS#Comment
-  property :tags, [String]
-
+class LinkedData::Property
+  include CouchRest::Model::Embeddable
+  
   property :term, String                              # Escaped vocabulary name suitable for inclusion in IRI
-  property :uri, String
-
+  property :label, String                             # RDFS#Label
+  property :expected_type, String                     # URI
+  property :comment, String                           # RDFS#Comment
+  property :enumerations
   property :deprecated, TrueClass, :default => false  # Deprecated properties may not be used 
                                                        # for future vocabularies            
-
-  belongs_to :vocabulary, :class_name => "LinkedData::Vocabulary"
-  belongs_to :expected_type, :class_name => "LinkedData::Type"
-  collection_of :synonyms, :class_name => 'LinkedData::Property'  # Props with same semantic meaning
-  timestamps!
-
-  validates_presence_of :term
-  validates_presence_of :vocabulary_id
-  validates_uniqueness_of :identifier, :view => 'all'
-  
-  before_create :generate_uri
-  before_create :generate_identifier
-
-  design do
-    view :by_label
-    view :by_term
-    view :by_uri
-    view :by_vocabulary_id
-    
-    view :tag_list,
-      :map =>
-        "function(doc) {
-          if (doc['type'] == 'LinkedData::Property' && doc.tags) {
-            doc.tags.forEach(function(tag) {
-              emit(tag, 1); 
-              });
-            }
-          }"
-  end
 
   def deprecated?
     self.deprecated
   end
 
-  def self.find_by_vocabulary_id(vocab_id)
-    self.by_vocabulary_id(:key => vocab_id)
-  end
-  
-private
-  def generate_uri
-    self.label ||= self.term
-    rdf_uri = RDF::URI.new(self.vocabulary.uri)/self.vocabulary.property_delimiter + self.term
-    self.uri = rdf_uri.to_s
-  end
-
-  def generate_identifier
-    self['identifier'] = self.class.to_s.split("::").last.downcase + '_' +
-                         self.vocabulary.authority + '_' + 
-                         self.vocabulary.term.downcase + '_' +
-                         escape_string(self.term.downcase) if new?
-  end
-
-  def escape_string(str)
-    str.gsub(/[^A-Za-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') 
-  end
+  # def term=(value)
+  #   self.term = value
+  #   sef.label ||= self.term
+  # end
 
 end
--- spec/models/linked_data/property_spec.rb
+++ spec/models/linked_data/property_spec.rb
@@ -3,72 +3,34 @@ require 'spec_helper'
 describe LinkedData::Property do
 
   before(:each) do
-    ns = LinkedData::Namespace.new("http://dcgov.civicopenmedia.us")
-    core = LinkedData::Collection.create!(:base_uri => ns.base_uri, 
-                                      :term => 'Core', 
-                                      :authority => ns.authority)
+    @ns = LinkedData::Namespace.new("http://dcgov.civicopenmedia.us")
                                       
-    ps = LinkedData::Collection.create!(:base_uri => ns.base_uri, 
-                                    :term => "public_safety",
-                                    :label => "Public Safety",
-                                    :authority => ns.authority)
-    
+    @ps_collection = LinkedData::Collection.create!(:base_uri => @ns.base_uri, 
+                                                    :term => "public_safety",
+                                                    :label => "Public Safety",
+                                                    :authority => @ns.authority)
+
     @xsd = ::LinkedData::Vocabulary.create!(:base_uri => "http://www.w3.org/2001",
-                                        :collection => core,
-                                        :term => "XMLSchema",
-                                        :property_delimiter => "#"
+                                            :collection => @ps_collection,
+                                            :term => "XMLSchema",
+                                            :property_delimiter => "#"
+                                            )
+                                            
+    @str_type = LinkedData::Type.create!(:vocabulary => @xsd, :term => "string", :label => "String")
+  end
+
+  it 'should properly set and get properties' do
+    @comment = "Crimes are classified by type, termed as an Offense"
+    @offense = LinkedData::Property.new(:term => "offense",
+                                        :label => "Offense",
+                                        :expected_type => @str,
+                                        :comment => @comment,
+                                        :deprecated => false
                                         )
-
-    @crime = LinkedData::Vocabulary.create!(:base_uri => ns.base_uri,
-                                        :collection => ps,
-                                        :term => "crime_reports",
-                                        :label => "Crime Reports",
-                                        :property_delimiter => "#"
-                                        )
-    
-    @int = LinkedData::Type.create!(:vocabulary => @xsd, :term => "integer", :label => "Integer")
-    @lng = LinkedData::Type.create!(:vocabulary => @xsd, :term => "long", :label => "Long")
-    @str = LinkedData::Type.create!(:vocabulary => @xsd, :term => "string", :label => "String")
-    
-    @offense = LinkedData::Property.new(:vocabulary => @crime, 
-                                         :term => "Offense", 
-                                         :expected_type => @str)
+                                        
+    @offense.deprecated?.should == false
+    @offense.comment.should == @comment
   end
-  
-  it 'should fail to initialize instance without both a label and namespace' do
-    lambda { LinkedData::Property.create!() }.should raise_error
-    lambda { LinkedData::Property.create!(:label => @label) }.should raise_error
-    lambda { LinkedData::Property.create!(:vocabulary => @crime) }.should raise_error
-  end
-  
-  it 'should save and generate an identifier correctly' do
-    @method = LinkedData::Property.new(:vocabulary => @crime, 
-                                       :term => "Method", 
-                                       :term => "method", 
-                                       :expected_type => @str)
-                                         
-    lambda { @method.save! }.should change(LinkedData::Property, :count).by(1)
-  end
-
-  it 'should generate a Label view and return results correctly' do
-    @method = LinkedData::Property.create!(:vocabulary => @crime, 
-                                       :term => "method", 
-                                       :label => "Method", 
-                                       :expected_type => @str)
-
-    @prop = LinkedData::Property.by_label(:key => "Method")
-    @prop.rows[0].id.should == 'property_civicopenmedia_us_dcgov_crime_reports_method'
-  end
-
-  it 'should generate a URI for the new property' do
-    @method = LinkedData::Property.create!(:vocabulary => @crime,
-                                           :term => "method", 
-                                           :label => "Method", 
-                                           :expected_type => @str)
-
-    @prop = LinkedData::Property.get(@method.id)
-    @prop.uri.should == 'http://civicopenmedia.us/dcgov/vocabularies/crime_reports#method'
-  end
-  
+   
 end
 

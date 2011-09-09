d90eab7d5b8ae72db397a793a3fb12fa1d3efa6c
Dan Thomas <dan.thomas@ipublic.org>
2011-09-08 21:32:26 -0400

    Add: methods to support metadata and document counts

--- app/models/linked_data/data_resource.rb
+++ app/models/linked_data/data_resource.rb
@@ -1,18 +1,47 @@
 class LinkedData::DataResource < CouchRest::Model::Base
   
-  use_database TYPES_DATABASE
-  
-  property :database
-  property :term
-  property :label
+  use_database STAGING_DATABASE
   
   belongs_to :vocabulary, :class_name => "LinkedData::Vocabulary"
+  belongs_to :creator_contact, :class_name => "VCard::VCard"
+  belongs_to :publisher_contact, :class_name => "VCard::VCard"
+
+  property :uri
+  property :term
+  property :label
+
+  timestamps!
 
   validates_presence_of :term
-  validates_presence_of :database
-  validates_presence_of :vocabulary_id
+
+  def publisher
+    VCard::Vcard.get(self.publisher_contact_id)
+  end
+
+  def creator
+    VCard::Vcard.get(self.creator_contact_id)
+  end
   
+  def raw_records(view_opts={})
+    OpenMedia::RawRecord.by_data_resource_id({:key=>self.id}.merge(view_opts))
+  end  
+
+  def unpublished_raw_records(view_opts={})
+    OpenMedia::RawRecord.by_unpublished({:key=>self.id}.merge(view_opts))
+  end  
+  
+  def raw_record_count
+    OpenMedia::RawRecord.by_data_resource_id(:key=>self.id, :include_docs=>false)['rows'].size
+  end
+
+  def published_raw_record_count(view_opts={})
+    OpenMedia::RawRecord.by_data_resource_id_and_published(:startkey=>[self.id], :endkey=>[self.id, {}], :include_docs=>false)['rows'].size
+  end
+  
+  def create_vocabulary
     
+  end
+  
   def create_design_doc
     raise "You must specify a database" if self.database.nil?
     raise "You must specify a term" if self.term.nil?
@@ -34,7 +63,19 @@ class LinkedData::DataResource < CouchRest::Model::Base
         })
   end
   
-  def bulk_save
+  def publish!
+    # generate ETL ctl file from dataset and source definition
+    ctl = "source :in, {:datasource=>'#{self.id}'}, ["+
+      self.source_properties.collect{|p| p.identifier}.collect{|p| ":#{p}"}.join(',') + "]\n"
+    ctl << "destination :out, {:rdfs_class=>'#{self.rdfs_class_uri}'}, {:order=>[" +
+      self.rdfs_class.properties.collect{|p| p.identifier}.collect{|p| ":#{p}"}.join(',') + "]}\n"
+
+    ETL::Engine.init(:datasource=>self)
+    ETL::Engine.process_string(self, ctl)
+    ETL::Engine.import = nil
+    metadata.update!(:modified=>DateTime.now)
+    ETL::Engine.rows_written
   end
+
   
 end
--- spec/models/linked_data/data_resource_spec.rb
+++ spec/models/linked_data/data_resource_spec.rb
@@ -3,8 +3,17 @@ require 'spec_helper'
 describe LinkedData::DataResource do
 
   before(:each) do
-    @dr_term = "awesome"
+    @dr_term = "reported_crimes"
     @dr = LinkedData::DataResource.new(:database => TYPES_DATABASE, :term => @dr_term)
+  end
+  
+  describe "initialization" do
+    it 'should fail to initialize instance without term property' do
+      @dr = LinkedData::DataResource.new
+      @dr.should_not be_valid
+      @dr.errors[:term].should_not be_nil
+      lambda { LinkedData::DataResource.create!(:term => @dr_term) }.should_not raise_error
+    end
   end
   
   describe "class methods" do
@@ -14,11 +23,20 @@ describe LinkedData::DataResource do
         @res["id"].should eql("_design/#{@dr_term}")
       end
     end
-    
   end
   
   describe "basics" do
+    it "should create a Vocabulary from source data content" do
+    end
+    
+    it "should extract and parse records from source data content" do
+    end
+    
+    it "should load raw records into CouchDB from source data content" do
+    end
+    
+    it "should transform raw records into published data sets" do 
+    end
   end
   
 end
-

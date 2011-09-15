8a39d17d0b176650d03b06ebfd6b3eb57b042783
Dan Thomas <dan.thomas@ipublic.org>
2011-09-15 18:46:38 -0400

    Add: type_from_data_value method and associated regular expressions

--- app/models/linked_data/parser.rb
+++ app/models/linked_data/parser.rb
@@ -3,6 +3,17 @@ require 'digest/md5'
 # Parent class for source data parsers
 class LinkedData::Parser
   
+  INTEGER_REGEX = /^[-+]?\d+([,]\d+)*$/
+  FLOAT_REGEX = /[-+]?[0-9]*\.[0-9]+/
+  TWENTY_FOUR_HOUR_TIME_REGEX = /^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])(:([0-5]?[0-9]))?$/
+  TWELVE_HOUR_TIME_REGEX = /^([1-9]|1[0-2]|0[1-9]){1}(:[0-5][0-9]){2}([aApP][mM]){1}$/
+  CSV_DATE_REGEX = / \A(?: (\w+,?\s+)?\w+\s+\d{1,2},?\s+\d{2,4} | \d{4}-\d{2}-\d{2} )\z /x
+  US_DATE_REGEX = /^(([1-9])|(0[1-9])|(1[0-2]))\/(([1-9])|(0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))\/((\d{2})|(\d{4}))$/
+  DATE_TIME_REGEX = /^([0]\d|[1][0-2])\/([0-2]\d|[3][0-1])\/([2][01]|[1][6-9])\d{2}(\s([0]\d|[1][0-2])(\:[0-5]\d){1,2})*\s*([aApP][mM]{0,2})?$/
+  # FLEX_DATE_TIME_REGEX = /^(([1-9])|(0[1-9])|(1[0-2]))\/(([1-9])|(0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))\/((\d{2})|(\d{4}))([1-9]|1[0-2]|0[1-9]){2}(:[0-5][0-9][aApP][mM]){1}$/
+  CSV_DATE_TIME_REGEX = / \A(?: (\w+,?\s+)?\w+\s+\d{1,2}\s+\d{1,2}:\d{1,2}:\d{1,2},?\s+\d{2,4} | \d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2} )\z /x
+  BOOLEAN_REGEX = /^(true)|(false)$/i
+  
   attr_accessor :property_constants
   attr_reader :data_resource, :source_file_name, :properties
 
@@ -12,6 +23,32 @@ class LinkedData::Parser
   
   def parse
     raise "abstract method - please override"
+  end
+  
+  def type_from_data_value(data)
+    value = data.to_s
+    if value =~ FLOAT_REGEX
+      type_uri = RDF::XSD.float.to_s      
+    elsif value =~ INTEGER_REGEX
+      type_uri = RDF::XSD.integer.to_s
+    elsif value =~ TWENTY_FOUR_HOUR_TIME_REGEX
+      type_uri = RDF::XSD.time.to_s      
+    elsif value =~ TWELVE_HOUR_TIME_REGEX
+      type_uri = RDF::XSD.time.to_s      
+    elsif value =~ CSV_DATE_REGEX
+      type_uri = RDF::XSD.date.to_s      
+    elsif value =~ US_DATE_REGEX
+      type_uri = RDF::XSD.date.to_s      
+    elsif value =~ CSV_DATE_TIME_REGEX
+      type_uri = RDF::XSD.dateTime.to_s      
+    elsif value =~ DATE_TIME_REGEX
+      type_uri = RDF::XSD.dateTime.to_s      
+    elsif value =~ BOOLEAN_REGEX
+      type_uri = RDF::XSD.boolean.to_s      
+    else
+      type_uri = RDF::XSD.string.to_s      
+    end
+    type_uri
   end
   
 end
--- spec/models/linked_data/parser_spec.rb
+++ spec/models/linked_data/parser_spec.rb
@@ -7,7 +7,50 @@ describe LinkedData::Parser do
   end
   
   describe "class methods" do
-
+    describe ".type_from_data_value" do
+      it "should identify integer types" do
+        @parser.type_from_data_value(0).should == RDF::XSD.integer.to_s
+        @parser.type_from_data_value(-0).should == RDF::XSD.integer.to_s
+        @parser.type_from_data_value(12345).should == RDF::XSD.integer.to_s
+        @parser.type_from_data_value(-12345).should == RDF::XSD.integer.to_s
+        @parser.type_from_data_value("12,345").should == RDF::XSD.integer.to_s
+        @parser.type_from_data_value("-12,345").should == RDF::XSD.integer.to_s
+      end
+      it "should identify float types" do
+        @parser.type_from_data_value(0.0).should == RDF::XSD.float.to_s
+        @parser.type_from_data_value(-0.0).should == RDF::XSD.float.to_s
+        @parser.type_from_data_value(123.45).should == RDF::XSD.float.to_s
+        @parser.type_from_data_value(-123.45).should == RDF::XSD.float.to_s
+        @parser.type_from_data_value("10,123.45").should == RDF::XSD.float.to_s
+        @parser.type_from_data_value("-10,123.45567").should == RDF::XSD.float.to_s
+      end
+      it "should identify time types" do
+        @parser.type_from_data_value("12:30:45 pm").should == RDF::XSD.time.to_s
+        @parser.type_from_data_value("16:40").should == RDF::XSD.time.to_s
+        @parser.type_from_data_value("16:40:32").should == RDF::XSD.time.to_s
+      end
+      it "should identify date types" do
+        @parser.type_from_data_value("04/04/1964").should == RDF::XSD.date.to_s
+        @parser.type_from_data_value("1/1/1964").should == RDF::XSD.date.to_s
+        @parser.type_from_data_value("10/23/2001").should == RDF::XSD.date.to_s
+        @parser.type_from_data_value("1/1/64").should == RDF::XSD.date.to_s
+        @parser.type_from_data_value("10/23/01").should == RDF::XSD.date.to_s
+      end
+      it "should identify dateTime types" do
+        @parser.type_from_data_value("04/04/1964 12:59 AM").should == RDF::XSD.dateTime.to_s
+        @parser.type_from_data_value("10/10/2010 04:15 PM").should == RDF::XSD.dateTime.to_s
+        @parser.type_from_data_value("5/16/2011 2:02:23 PM").should == RDF::XSD.dateTime.to_s
+      end
+      it "should identify boolean types" do
+        @parser.type_from_data_value("TRUE").should == RDF::XSD.boolean.to_s
+        @parser.type_from_data_value("false").should == RDF::XSD.boolean.to_s
+      end
+      it "should identify string types" do
+        @parser.type_from_data_value("it was a dark and stormy night").should == RDF::XSD.string.to_s
+        @parser.type_from_data_value("12buckle my shoe").should == RDF::XSD.string.to_s
+        @parser.type_from_data_value("12 buckle my shoe").should == RDF::XSD.string.to_s
+      end
+    end
   end
   
   describe "basics" do

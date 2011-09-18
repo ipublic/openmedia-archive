62944487d585495fc8c709257db64cfd7054a9b6
Dan Thomas <dan.thomas@ipublic.org>
2011-09-18 09:31:46 -0400

    Fix: removed extra flatten from ArgumentError test

--- app/models/geo_json/position.rb
+++ app/models/geo_json/position.rb
@@ -1,14 +1,17 @@
 class GeoJson::Position < Array
+  
+  attr_reader :coordinates
 
   def initialize(*args)
     tuple = args.flatten
     
     # Note: GeoJson supports > 2 or 3 coordinates for geograhic position and ignores > 3 values
     raise ArgumentError, 
-      "Requires either two or three ordered numeric arguments: x, y, z" unless tuple.flatten.length.between?(2,3)
+      "Requires either two or three ordered numeric arguments: x, y, z" unless tuple.length.between?(2,3)
     raise ArgumentError,
       "Requires either two or three ordered numeric arguments: x, y, z" unless tuple.all? { |e| Numeric === e }
-    super tuple
-  end
 
+    super
+    @coordinates = tuple
+  end
 end

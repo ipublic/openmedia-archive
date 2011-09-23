c4ffbc975b5c9dec53d34e46ae32532a4405b434
Dan Thomas <dan.thomas@ipublic.org>
2011-09-22 20:38:35 -0400

    initial commit

---
+++ app/models/linked_data/data_source.rb
@@ -0,0 +1,91 @@
+class LinkedData::DataSource < CouchRest::Model::Base
+  
+  use_database STAGING_DATABASE
+  unique_id :identifier
+  
+  CSV_SOURCE_TYPE = "csv"
+  SHAPEFILE_SOURCE_TYPE = "shapefile"
+  URL_SOURCE_TYPE = "url"
+  
+  property :term, String
+  property :identifier, String
+  property :label, String
+  property :authority, String
+  
+  property :extract_sets do
+    property :extract_serial_number, String
+    property :record_count, Integer
+    property :extracted_at, Time
+  end
+
+  timestamps!
+
+  validates_presence_of :term
+  validates_presence_of :authority
+  
+  ## Callbacks
+  before_create :generate_identifier
+
+  design do
+    view :by_term
+    view :by_label
+  end
+  
+  def last_extract(view_opts={})
+    unless self.extract_sets.nil?
+      LinkedData::RawRecord.by_extract_serial_number({:key=>self.extract_sets.last.extract_serial_number}.merge(view_opts))
+    end
+  end
+
+  def raw_records(view_opts={})
+    LinkedData::RawRecord.by_data_source_id({:key=>self.id}.merge(view_opts))
+  end  
+
+  def unpublished_raw_records(view_opts={})
+    LinkedData::RawRecord.by_unpublished({:key=>self.id}.merge(view_opts))
+  end  
+  
+  def raw_record_count
+    LinkedData::RawRecord.by_data_source_id(:key=>self.id, :include_docs=>false)['rows'].size
+  end
+
+  def published_raw_record_count(view_opts={})
+    LinkedData::RawRecord.by_data_source_id_and_published(:startkey=>[self.id], :endkey=>[self.id, {}], :include_docs=>false)['rows'].size
+  end
+  
+  def self.extract_serial_number
+    ::Digest::MD5.hexdigest(Time.now.to_i.to_s + rand.to_s).to_s
+  end
+  
+  def extract!(records)
+    raise "DataSource must be saved to database first" if self.identifier.nil?
+    esn = LinkedData::DataSource.extract_serial_number
+    ts = Time.now
+    extract_prop_set = {:extract_serial_number => esn, :data_source_id => self.identifier, 
+                        :created_at => ts, :updated_at => ts}
+    
+    recs_saved = 0
+    records.each do |rec|
+      LinkedData::RawRecord.database.bulk_save_doc(rec.merge(extract_prop_set))
+      recs_saved += 1
+      LinkedData::RawRecord.database.bulk_save if recs_saved%500 == 0              
+    end
+    LinkedData::RawRecord.database.bulk_save
+    extract_sets << Hash[:extract_serial_number => esn, :record_count => recs_saved, :extracted_at => ts]
+    self.save
+    extract_sets.last
+  end
+
+private
+  def generate_identifier
+    self.label ||= self.term
+    self['identifier'] = self.class.to_s.split("::").last.downcase + '_' +
+                         self.authority + '_' + 
+                         escape_string(self.term.downcase) if new?
+  end
+
+  def escape_string(str)
+    str.gsub(/[^A-Za-z0-9]/,'_').squeeze('_')
+  end
+  
+end
---
+++ spec/models/linked_data/data_source_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+
+describe LinkedData::DataSource do
+
+  before(:each) do
+    @ns = LinkedData::Namespace.new("http://dcgov.civicopenmedia.us")
+    @ds_term = "reported_crimes"
+    @ds = LinkedData::DataSource.new(:authority => @ns.authority, :term => @ds_term)
+    @ds_id = "datasource_civicopenmedia_us_dcgov_reported_crimes"
+  end
+
+  describe "initialization" do
+    it 'should fail to initialize instance without term and authority properties' do
+      @ds = LinkedData::DataSource.new
+      @ds.should_not be_valid
+      @ds.errors[:term].should_not be_nil
+      @ds.errors[:authority].should_not be_nil
+    end
+    
+    it 'should save and generate an identifier correctly' do
+      lambda { LinkedData::DataSource.create!(:authority => @ns.authority, :term => @ds_term) }.should_not raise_error
+      saved_dr = LinkedData::DataSource.by_term(:key => @ds_term)
+      saved_dr.first.identifier.should == @ds_id
+    end
+  end
+  
+  describe "class methods" do
+    describe ".extract_serial_number" do
+      it "should generate a unique serial number using MD5 hash seeded by current time and random number" do
+        @sn1 = LinkedData::DataSource.extract_serial_number
+        @sn2 = LinkedData::DataSource.extract_serial_number
+        @sn1.should_not == @sn2
+      end
+    end
+  end
+  
+  describe "instance methods" do
+    before(:each) do
+      STAGING_DATABASE.recreate! rescue nil
+      @csv_filename = File.join(FIXTURE_PATH, 'crime_incidents_current.csv')
+      @parser = LinkedData::CsvParser.new(@csv_filename, {:header_row => true})
+    end
+    
+    describe ".extract!" do
+      it "should raise an error if DataSource doc isn't saved" do
+        lambda{@ds.extract!(@parser.records)}.should raise_error
+      end
+      
+      it "should store all parsed records in the Staging db" do
+        @ds.save
+        @stats = @ds.extract!(@parser.records)
+        @stats.record_count.should == 304
+      end
+    end
+  end
+
+  describe "views" do
+    before(:each) do
+      STAGING_DATABASE.recreate! rescue nil
+
+      @csv_ds = LinkedData::DataSource.create!(:authority => @ns.authority, :term => @ds_term)
+      @csv_filename = File.join(FIXTURE_PATH, 'crime_incidents_current.csv')
+      @csv_parser = LinkedData::CsvParser.new(@csv_filename, {:header_row => true})
+      @csv_stats = @csv_ds.extract!(@csv_parser.records)
+
+      @shp_ds_term = "dc_fire_stations"
+      @shp_ds = LinkedData::DataSource.create!(:authority => @ns.authority, :term => @shp_ds_term)
+
+      @shapefile_name = File.join(FIXTURE_PATH, 'FireStnPt.zip')
+      @shp_parser = LinkedData::ShapefileParser.new(@shapefile_name)
+      @shp_stats = @shp_ds.extract!(@shp_parser.records)
+    end
+
+    it "should return accurate counts for RawRecord and each DataSource" do
+      LinkedData::DataSource.all.length.should == 2
+      LinkedData::RawRecord.all.length.should == 339
+      @csv_ds.raw_record_count.should == 304
+      @csv_ds.last_extract.length.should == 304
+      @shp_ds.raw_record_count.should == 35
+    end
+
+  end
+end

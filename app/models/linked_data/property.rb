b4deec7dc9d3c091ae402000376359607549fd0b
Dan Thomas <dan.thomas@ipublic.org>
2011-09-24 08:33:44 -0400

    Fix: remove commented out cruft

--- app/models/linked_data/property.rb
+++ app/models/linked_data/property.rb
@@ -8,24 +8,11 @@ class LinkedData::Property
   property :comment, String                           # RDFS#Comment
   property :deprecated, TrueClass, :default => false  # Deprecated properties may not be used 
                                                        # for future vocabularies            
-
-  # def initialize(options={})
-  #   super(options)
-  #   raise "you must provide a term value" if self.term.nil?
-  # end
-
   def term=(val)
     write_attribute('term', val)
     write_attribute('label', val) if read_attribute('label').nil?
   end
   
-  # def label
-  #   if read_attribute('label').nil?
-  #     write_attribute('label', read_attribute('term')) unless read_attribute('term').nil?
-  #   end
-  #   read_attribute('label')
-  # end
-
   def deprecated?
     self.deprecated
   end

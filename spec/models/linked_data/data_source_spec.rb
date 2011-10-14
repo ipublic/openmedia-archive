0e117c95c9f85c259bec22119814b9b3afdefda2
Dan Thomas <dan.thomas@ipublic.org>
2011-10-14 17:26:35 -0400

    Add: check for properties array values Fix: optimization

--- spec/models/linked_data/data_source_spec.rb
+++ spec/models/linked_data/data_source_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe LinkedData::DataSource do
 
-  before(:each) do
+  before(:all) do
     @ns = LinkedData::Namespace.new("http://dcgov.civicopenmedia.us")
     @ds_term = "reported_crimes"
     @ds = LinkedData::DataSource.new(:authority => @ns.authority, :term => @ds_term)
     @ds_id = "datasource_civicopenmedia_us_dcgov_reported_crimes"
+
+    # STAGING_DATABASE.recreate! rescue nil
+    @csv_filename = File.join(FIXTURE_PATH, 'crime_incidents_current.csv')
+    @parser = LinkedData::CsvParser.new(@csv_filename, {:header_row => true})
   end
 
   describe "initialization" do
@@ -16,46 +20,51 @@ describe LinkedData::DataSource do
       @ds.errors[:term].should_not be_nil
       @ds.errors[:authority].should_not be_nil
     end
+
+    it "should accept an array of LinkedData::Property for source data" do
+      @ds.properties = @parser.properties
+      @ds.properties.length.should == 22
+      @ds.properties.first.should be_a(LinkedData::Property)
+      @ds.properties.first.term.should == "NID"
+    end
     
     it 'should save and generate an identifier correctly' do
       lambda { LinkedData::DataSource.create!(:authority => @ns.authority, :term => @ds_term) }.should_not raise_error
-      saved_dr = LinkedData::DataSource.by_term(:key => @ds_term)
-      saved_dr.first.identifier.should == @ds_id
+      saved_ds = LinkedData::DataSource.get @ds_id
+      saved_ds.identifier.should == @ds_id
+    end
+
+    it 'should present a LinkedData::DataSource when searching by key' do
+      saved_ds = LinkedData::DataSource.by_term(:key => @ds_term)
+      saved_ds.first.identifier.should == @ds_id
     end
   end
   
   describe "class methods" do
     describe ".serial_number" do
       it "should generate a unique serial number using MD5 hash seeded by current time and random number" do
-        @sn1 = LinkedData::DataSource.serial_number
-        @sn2 = LinkedData::DataSource.serial_number
-        @sn1.should_not == @sn2
+        LinkedData::DataSource.serial_number.should_not == LinkedData::DataSource.serial_number
       end
     end
   end
   
   describe "instance methods" do
-    before(:each) do
-      STAGING_DATABASE.recreate! rescue nil
-      @csv_filename = File.join(FIXTURE_PATH, 'crime_incidents_current.csv')
-      @parser = LinkedData::CsvParser.new(@csv_filename, {:header_row => true})
-    end
-    
     describe ".extract!" do
       it "should raise an error if DataSource doc isn't saved" do
         lambda{@ds.extract!(@parser.records)}.should raise_error
       end
-      
+
       it "should store all parsed records in the Staging db" do
-        @ds.save
-        @stats = @ds.extract!(@parser.records)
+        # @ds.save
+        saved_ds = LinkedData::DataSource.get @ds_id
+        @stats = saved_ds.extract!(@parser.records)
         @stats.record_count.should == 304
       end
     end
   end
 
   describe "views" do
-    before(:each) do
+    before(:all) do
       STAGING_DATABASE.recreate! rescue nil
 
       @csv_ds = LinkedData::DataSource.create!(:authority => @ns.authority, :term => @ds_term)
